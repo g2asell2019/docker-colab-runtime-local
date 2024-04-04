0e9e0997ba4f0737a2946209ff1a94ad36e56c9d
g2asell2019 <51025883+g2asell2019@users.noreply.github.com>
2024-04-04 11:46:31 +0700

    Update start-jupyter.sh

--- start-jupyter.sh
+++ start-jupyter.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | \
+  sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && \
+  echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | \
+  sudo tee /etc/apt/sources.list.d/ngrok.list && \
+  sudo apt update && sudo apt install ngrok
+
+nohup ngrok http 80 --log=stdout > ngrok.log &
+
 jupyter notebook --notebook-dir=/content --ip='*' \
     --port=8888 --no-browser --allow-root \
     --NotebookApp.allow_origin='https://colab.research.google.com' \
